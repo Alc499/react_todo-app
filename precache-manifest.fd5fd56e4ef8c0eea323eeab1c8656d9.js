6361123fbea4a38c3c5e80dacc9fa5620c7a9855
Alc499 <Ali.vitchenko@gmail.com>
2019-07-26 17:33:03 +0300

    Updates

---
+++ precache-manifest.fd5fd56e4ef8c0eea323eeab1c8656d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d0cfd517a8e0955550e78d972eb7e9",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "6ba227586d019bd463e9",
+    "url": "/react_todo-app/static/css/main.abae98c4.chunk.css"
+  },
+  {
+    "revision": "8b6498524d2785fb1509",
+    "url": "/react_todo-app/static/js/2.f2b4a267.chunk.js"
+  },
+  {
+    "revision": "6ba227586d019bd463e9",
+    "url": "/react_todo-app/static/js/main.e3b9fb78.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
